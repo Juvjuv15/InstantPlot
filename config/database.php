a45afe2447d3c669f5f39a61c9017c2494bd119a
Juvjuv15 <recitasj@gmail.com>
2021-05-15 12:37:08 +0800

    update db and env

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'us-cdbr-east-03.cleardb.com'),
+            'host' => env('DB_HOST', 'http://localhost:8080'),
             'port' => env('DB_PORT', '3307'),
-            'database' => env('DB_DATABASE', 'heroku_df5e6664d358567'),
-            'username' => env('DB_USERNAME', 'b00397db4db917'),
-            'password' => env('DB_PASSWORD', 'f289153a'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
