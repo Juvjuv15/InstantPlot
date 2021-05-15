e90da6d9dae0d1d7a145f33143a6c6827bfd791b
Juvjuv15 <recitasj@gmail.com>
2021-05-15 21:34:01 +0800

    updatedb2

--- config/database.php
+++ config/database.php
@@ -4,10 +4,10 @@ $cleardb_server = $cleardb_url["host"];
 $cleardb_username = $cleardb_url["user"];
 $cleardb_password = $cleardb_url["pass"];
 $cleardb_db = substr($cleardb_url["path"],1);
-$active_group = 'default';
-$query_builder = TRUE;
+//$active_group = 'default';
+//$query_builder = TRUE;
 // Connect to DB
-$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+//$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
 return [
 
