6495c06e5844e5a19acd87e9d5da4d07c58225e2
Juvjuv15 <recitasj@gmail.com>
2021-05-16 01:19:54 +0800

    delete comment in app.ph

--- config/app.php
+++ config/app.php
@@ -1,16 +1,5 @@
 <?php
 
-cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = $cleardb_url["host"] ?? null;
-$cleardb_username = $cleardb_url["user"] ?? null;
-$cleardb_password = $cleardb_url["pass"] ?? null;
-$cleardb_db = substr($cleardb_url["path"],1) ?? null;
-$active_group = 'default';
-$query_builder = TRUE;
-// Connect to DB
-$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
-
-
 return [
 
     'Uuid' => Webpatser\Uuid\Uuid::class,
