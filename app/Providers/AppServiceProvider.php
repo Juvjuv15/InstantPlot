47c87d202de536bac7033f1e134a56b220a4ec46
Juvjuv15 <recitasj@gmail.com>
2021-05-15 15:03:22 +0800

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Contracts\Routing\UrlGenerator;
+use Illuminate\Support\Facades\Route;
 
 
 class AppServiceProvider extends ServiceProvider
