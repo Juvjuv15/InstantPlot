043d1bdf20836350a08d03600881ebe2f6939633
Juvjuv15 <recitasj@gmail.com>
2021-05-15 21:44:35 +0800

    add null

--- config/database.php
+++ config/database.php
@@ -1,9 +1,9 @@
 <?php
 $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
-$cleardb_db = substr($cleardb_url["path"],1);
+$cleardb_server = $cleardb_url["host"] ?? null;
+$cleardb_username = $cleardb_url["user"] ?? null;
+$cleardb_password = $cleardb_url["pass"] ?? null;
+$cleardb_db = substr($cleardb_url["path"],1) ?? null;
 //$active_group = 'default';
 //$query_builder = TRUE;
 // Connect to DB
